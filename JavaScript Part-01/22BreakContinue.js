7f77ae464bfa1144e4543e2e509836c596911688
Abhishek Singh <91690267+isinghabhishek@users.noreply.github.com>
2023-01-31 23:18:18 +0530

    Update 22BreakContinue.js

--- JavaScript Part-01/22BreakContinue.js
+++ JavaScript Part-01/22BreakContinue.js
@@ -1,7 +1,7 @@
 
 // break keyword
 for (let i = 1; i <= 10; i++) {
-  if (i === 4) {// (i == 4) loop will break
+  if (i === 4) {// (i == 4) loop will break if (i == 4)
     break;
   }
   console.log(i);
