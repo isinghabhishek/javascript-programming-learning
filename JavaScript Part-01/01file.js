2b9a013692a02b37f904d9208f8ce7060e84fc09
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2024-09-09 22:47:27 +0530

    add: updating comment

--- JavaScript Part-01/01file.js
+++ JavaScript Part-01/01file.js
@@ -1,19 +1,25 @@
-// printing hello world in console
+//  IDEA: printing hello world in console
 
-// console.log() can print something on console
+// NOTE: 'filter method' will filter out value according to the give condition
+
+// BUG: 1. JavaScript is a Oops language
+// TODO:
+// FIXME:
+// BUG: Variables
+// TODO: console.log() can print something on console
 
 console.log("Hello World");
 console.log("Hello, World");
 
-// Single Line Comment: It is represented by // (double forward slash)
-// Multi-Line Comment: Slash represents it with asterisk symbol as /* write comment here */
+// NOTE: Single Line Comment: It is represented by // (double forward slash)
+// NOTE: Multi-Line Comment: Slash represents it with asterisk symbol as /* write comment here */
 
-// String Important Interview Question?
+// HACK: String Important Interview Question?
 
-// 1) What is the output of 10+20+"30" in JavaScript?
-// => 3030 because 10+20 will be 30. If there is numeric value before 
+// INFO: 1) What is the output of 10+20+"30" in JavaScript?
+// => 3030 because 10+20 will be 30. If there is numeric value before
 //    and after +, it treats as binary + (arithmetic operator).
 
-// 2) What is the output of "10"+20+30 in JavaScript?
-// => 102030 because after a string all the + will be treated as 
+// INFO:2) What is the output of "10"+20+30 in JavaScript?
+// => 102030 because after a string all the + will be treated as
 //    string concatenation operator (not binary +).
--- JavaScript Part-01/55FilterMethod.js
+++ JavaScript Part-01/55FilterMethod.js
@@ -1,24 +1,29 @@
-//   filter  Method
-// 'filter method' will filter out value according to the give condition
+//  IDEA: filter  Method
+// NOTE: 'filter method' will filter out value according to the give condition
 
+// BUG: 1. JavaScript is a Oops language
+// TODO: 2.
+// FIXME: 3.
+// BUG: 4. Variables
+// HACK: 5.
+// INFO: 6.
 
 // these be use to filter out the both even and odd numbers.
 // with the help of the filter function it stores only
 // that number which is odd.
 
-const numbers = [1,3,2,6,4,8];
-const isOdd = function(number){
-    return number%2!==0;
-}
+const numbers = [1, 3, 2, 6, 4, 8];
+const isOdd = function (number) {
+  return number % 2 !== 0;
+};
 const oddNumber = numbers.filter(isOdd);
 console.log(oddNumber);
-
 
 // with the help of the filter function it stores only
 // that number which is even.
 
 // simple above funtion using callback function & arrow function
-const evenNumbers = numbers.filter((number)=>{
-    return number%2===0;
+const evenNumbers = numbers.filter((number) => {
+  return number % 2 === 0;
 });
 console.log(evenNumbers);
